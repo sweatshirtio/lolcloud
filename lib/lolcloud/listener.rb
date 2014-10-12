a98b85288e28e75cc023b134fd739d84fbb27ee3
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-10-11 20:37:30 -0600

    Create listener

--- lib/lolcloud/listener.rb
+++ lib/lolcloud/listener.rb
@@ -1,14 +1,31 @@
+require 'fileutils'
+require 'listen'
+
 module Lolcloud
   class Listener
+    attr_reader :sync_dirs, :default_sync_dir
+
     def initialize(options = {})
+      @default_sync_dir = File.join(Dir.home, 'lolcloud')
+      @sync_dirs = initialize_sync_dirs(options[:sync_dirs] || [])
     end
 
     def listen_up
-      Listen.to(sync_dir) do |modified, added, removed|
+      listener = Listen.to(sync_dirs) do |modified, added, removed|
         puts "modified absolute path: #{modified}"
         puts "added absolute path: #{added}"
         puts "removed absolute path: #{removed}"
       end
+
+      listener.start
+      sleep
+    end
+
+    private
+
+    def initialize_sync_dirs(dirs)
+      dirs << default_sync_dir
+      FileUtils.mkpath dirs
     end
   end
 end
